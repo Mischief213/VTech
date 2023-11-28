27089fa32b8df0576f2c572bb2c56e084cfa72d2
imran <imranroszaide89@gmail.com>
2023-11-28 14:53:11 +0800

    commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+var searchInput = document.querySelector('.search-container');
+var cancelOrder = document.querySelector('.cancel');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -15,10 +17,12 @@ function showLoginPopup() {
     wrapper.classList.add('active-popup');
 }
 
-
 function toggleSearch() {
-    var searchInput = document.querySelector('.search-container');
     searchInput.classList.toggle('active');
+}
+
+function toggleCancel() {
+    cancelOrder.classList.toggle('active');
 }
 
 document.addEventListener("DOMContentLoaded", function () {
