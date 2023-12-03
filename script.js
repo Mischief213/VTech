220205e37e658af21901d0d1b507ef2017aa4865
imran <imranroszaide89@gmail.com>
2023-12-03 14:19:10 +0800

    commit

--- script.js
+++ script.js
@@ -29,4 +29,58 @@ document.addEventListener("DOMContentLoaded", function () {
     showLoginPopup();
 });
 
+// Check if a section is stored in localStorage and display it
+document.addEventListener("DOMContentLoaded", function () {
+    let storedSection = localStorage.getItem('selectediPhoneSection');
+    if (storedSection) {
+        let section = document.getElementById(storedSection);
+        if (section) {
+            showOnlyiPhone(section);
+        }
+    }
+});
+
+let iPhone14 = document.getElementById('iPhone14');
+let iPhone13 = document.getElementById('iPhone13');
+let iPhone12 = document.getElementById('iPhone12');
+let iPhone11 = document.getElementById('iPhone11');
+let iPhoneX = document.getElementById('iPhoneX');
+let boxTitle = document.querySelector('.box-title');
+
+function showOnlyiPhone(section) {
+    // Set all iPhone sections to be initially hidden
+    iPhone14.style.display = 'none';
+    iPhone13.style.display = 'none';
+    iPhone12.style.display = 'none';
+    iPhone11.style.display = 'none';
+    iPhoneX.style.display = 'none';
+
+    // Display only the selected iPhone section
+    section.style.display = 'block';
+
+    // Move the selected section to the top of the page
+    section.parentNode.insertBefore(section, section.parentNode.firstChild);
+
+    // Move the box-title to the top
+    boxTitle.parentNode.insertBefore(boxTitle, boxTitle.parentNode.firstChild);
+
+    // Store the selected section in localStorage
+    localStorage.setItem('selectediPhoneSection', section.id);
+}
+
+function showAll() {
+    // Set all iPhone sections to be initially visible
+    iPhone14.style.display = 'block';
+    iPhone13.style.display = 'block';
+    iPhone12.style.display = 'block';
+    iPhone11.style.display = 'block';
+    iPhoneX.style.display = 'block';
+
+    // Move the box-title to the top
+    boxTitle.parentNode.insertBefore(boxTitle, boxTitle.parentNode.firstChild);
+
+    // Remove the stored section from localStorage
+    localStorage.removeItem('selectediPhoneSection');
+}
+
 
